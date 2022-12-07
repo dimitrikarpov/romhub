151c95213b01c449cfb30fe4215faefc3d1a5da0
dimitrikarpov <karpov.dimon@gmail.com>
2022-12-07 11:36:17 +0500

    add controls descripiton

--- pages/emulator.tsx
+++ pages/emulator.tsx
@@ -68,6 +68,35 @@ export default function Emulator({ rom, url }: Props) {
       <div className={styles.pageRomMeta}>
         {rom?.description && <p>{rom.description}</p>}
       </div>
+
+      <div>
+        <h4>controls</h4>
+        <div className={styles.controlsGrid}>
+          {Object.entries(keyConfig).map(([gamepadKey, keyboardKey]) => (
+            <>
+              <div className={styles.controlsGamepadKeyWrapper}>
+                {gamepadKey}
+              </div>
+              <div>{keyboardKey}</div>
+            </>
+          ))}
+        </div>
+      </div>
     </div>
   )
+}
+
+const keyConfig = {
+  "⬆️": "up",
+  "⬇️": "down",
+  "⬅️": "left",
+  "➡️": "right",
+  start: "enter",
+  select: "space",
+  A: "x",
+  B: "z",
+  X: "s",
+  Y: "a",
+  L: "q",
+  R: "w",
 }
