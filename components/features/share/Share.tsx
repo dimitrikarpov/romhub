9ef21510b3aed76083929ac2f49bd947b2130761
dimitrikarpov <karpov.dimon@gmail.com>
2023-02-12 10:25:34 +0500

    fix doubled slash at share url

--- components/features/share/Share.tsx
+++ components/features/share/Share.tsx
@@ -12,9 +12,9 @@ export const Share: React.FunctionComponent<Props> = (props) => {
   const { asPath } = useRouter()
   const inputRef = useRef<HTMLInputElement>(null)
   const path =
-    props && props?.type && props?.id ? `${props.type}/${props.id}` : asPath
+    props && props?.type && props?.id ? `/${props.type}/${props.id}` : asPath
 
-  const url = `${process.env.NEXT_PUBLIC_DOMAIN}/${path}`
+  const url = `${process.env.NEXT_PUBLIC_DOMAIN}${path}`
 
   const onCopy = async () => {
     inputRef?.current?.focus()
