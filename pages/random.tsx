415119f04d88d35357508eaf88cdf5cfc32abc6e
dimitrikarpov <karpov.dimon@gmail.com>
2023-02-28 17:16:07 +0500

    make random works on client side only

--- pages/random.tsx
+++ pages/random.tsx
@@ -1,24 +1,15 @@
 import React, { ReactElement } from "react"
 import Head from "next/head"
-import { UiRom } from "../types"
-import { NextPageWithLayout } from "./_app"
 import { Layout } from "@/components/pages/layout/Layout"
-import { dbQueries } from "@/lib/queries/dbQueries"
 import { RandomGrid } from "@/components/pages/random/RandomGrid"
 import { useRandomRomsQuery } from "@/lib/queries/react/useRandomRoms"
 import styles from "../styles/Random.module.css"
 import { useQueryClient } from "react-query"
 
-type Props = {
-  initialData: {
-    roms: UiRom[]
-  }
-}
-
-const Random: NextPageWithLayout<Props> = ({ initialData }) => {
+const Random = () => {
   const queryClient = useQueryClient()
 
-  const { data: roms } = useRandomRomsQuery({ initialData: initialData.roms })
+  const { data: roms } = useRandomRomsQuery({})
 
   const onRoll = () => {
     queryClient.invalidateQueries({
@@ -50,11 +41,3 @@ Random.getLayout = function getLayout(page: ReactElement) {
 }
 
 export default Random
-
-export async function getServerSideProps() {
-  const roms = await dbQueries.getRandomRoms(6)
-
-  return {
-    props: { initialData: { roms } },
-  }
-}
