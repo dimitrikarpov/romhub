f66f50b46c2f309671f6c95ade1a36285322a7fb
dimitrikarpov <karpov.dimon@gmail.com>
2023-05-15 18:02:39 +0500

    initial implementation

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next"
 import { getSession } from "next-auth/react"
-import { ReactElement, useState } from "react"
+import React, { ReactElement, useState } from "react"
 import { List } from "~/components/pages/history/List"
 import { Layout } from "~/components/pages/layout/Layout"
 import { Paginator } from "~/components/ui/paginator/Paginator"
@@ -8,33 +8,64 @@ import { useFetch } from "~/lib/fetcher"
 import prisma from "~/lib/prismadb"
 import { type GetPlaylistsEntries } from "~/lib/queries/db/getPlaylistsEntries"
 import { NextPageWithLayout } from "./_app"
+import { useInfiniteQuery } from "react-query"
+import { Playlist } from "@prisma/client"
+import { UiRom } from "../types"
+import { Item } from "~/components/pages/history/Item"
+import { Button } from "~/components/ui/button/button"
+
+type HistoryData = {
+  rom: UiRom
+  id: string
+  romId: string
+  playlistId: string
+  assignedAt: Date
+  playlist: Playlist
+}
 
 const History: NextPageWithLayout<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ id }) => {
-  const [skip, setSkip] = useState(0)
+  const {
+    isLoading,
+    isError,
+    data,
+    error,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["history"],
+    queryFn: async ({ pageParam = "" }) => {
+      const response = await fetch(
+        [
+          "/api/playlists/entries/infinite",
+          new URLSearchParams({ cursor: pageParam, playlistId: id }),
+        ].join("?"),
+      )
+      const data = await response.json()
 
-  const { data } = useFetch<GetPlaylistsEntries>({
-    url: "/api/playlists/entries",
-    search: {
-      playlistId: id,
-      skip,
-      take: 15,
-      orderBy: { assignedAt: "desc" },
+      return data
     },
+    getNextPageParam: (lastPage) => lastPage.nextCursor ?? false,
   })
 
+  console.log({ data })
   return (
-    <div>
-      {!!data?.data && <List entries={data.data} />}
-      <div className="px-0 py-12">
-        <Paginator
-          skip={skip}
-          setSkip={setSkip}
-          total={data?.total}
-          pageSize={15}
-        />
-      </div>
+    <div className="mx-auto my-0 max-w-[628px] pt-12 [&>*:not(:last-child)]:mb-8">
+      <h2 className="mb-2 mt-6 text-base font-light text-[#f1f1f1]">
+        Watch history
+      </h2>
+      {data &&
+        data.pages.map((group, i) => (
+          <React.Fragment key={i}>
+            {group.data.map((entry: HistoryData) => {
+              return <Item entry={entry} key={entry.id} />
+            })}
+          </React.Fragment>
+        ))}
+
+      {hasNextPage && <Button onClick={() => fetchNextPage()}>MOAR!!</Button>}
     </div>
   )
 }
